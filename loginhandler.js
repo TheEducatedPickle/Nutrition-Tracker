ef9fd7806d5bcf4be20e9399205dbc993031fa77
Priya Rajarathinam <cat@eduroam-169-233-197-36.ucsc.edu>
2019-01-19 13:25:26 -0800

    stuf

---
+++ loginhandler.js
@@ -0,0 +1,21 @@
+
+function createUser(email,password){
+    console.log("Creating user")
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+    // ...
+    });
+    
+
+}
+
+function signInUser(email,password){
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+}
