089eb458a419b617e02c97ac6082ef70040a7d52
Austin Yuan <sax202@gmail.com>
2019-01-19 14:20:16 -0800

    gradient nb

--- js/fetchData.js
+++ js/fetchData.js
@@ -7,7 +7,7 @@ var databasekey = "10SKlZqWe3IkC3ymxUWxzMrjgUfNFuixcuqY10gC";
 var dbnum = `01009`;
 var ntCategories = '&nutrients=205&nutrients=204&nutrients=208&nutrients=269';
 
-function searchItem(searchTerm="soylent") {
+function searchItem(searchTerm = "soylent") {
   //var query = `https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=${databasekey}${ntCategories}&ndbno=${dbnum}`;
   var query = `https://api.nal.usda.gov/ndb/search/?format=json&q=${searchTerm}&sort=r&max=25&offset=0&api_key=${databasekey}`;
   request.open('GET', query, true);
@@ -15,33 +15,40 @@ function searchItem(searchTerm="soylent") {
     console.log(searchTerm)
     var json = JSON.parse(this.response);
     if (request.status >= 200 && request.status < 400) {
-      console.log (formatTitle(json.list.item[0].name));
-      console.log (json.list.item.map(e => formatTitle(e.name)));
+      console.log(formatTitle(json.list.item[0].name));
+      console.log(json.list.item.map(e => formatTitle(e.name)));
     } else {
       console.log('Request timeout');
     }
   }
+
+  request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 200) {
+      return request.responseText;
+    }
+  }
   request.send();
+  return request.onreadystatechange();
 }
 
-function getSuggestions(searchTerm="chicken") {
+function getSuggestions(searchTerm = "chicken") {
   let suggestions = searchItem(searchTerm);
   console.log(suggestions);
 }
 
 function listItems() {
-    var query = `https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=${databasekey}${ntCategories}&ndbno=${dbnum}`;
-    //var query = `https://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=n&max=25&offset=0&api_key=${databasekey}`;
-    request.open('GET', query, true);
-    request.onload = function () {
-      var json = JSON.parse(this.response);
-      if (request.status >= 200 && request.status < 400) {
-        console.log(json.report.foods[0].nutrients[0].value);
-      } else {
-        console.log('Request timeout');
-      }
+  var query = `https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=${databasekey}${ntCategories}&ndbno=${dbnum}`;
+  //var query = `https://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=n&max=25&offset=0&api_key=${databasekey}`;
+  request.open('GET', query, true);
+  request.onload = function () {
+    var json = JSON.parse(this.response);
+    if (request.status >= 200 && request.status < 400) {
+      console.log(json.report.foods[0].nutrients[0].value);
+    } else {
+      console.log('Request timeout');
     }
-    request.send();
+  }
+  request.send();
 }
 
 function formatTitle(text) {  //Removes UPC and formats capitalization
