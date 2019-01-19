73d36b6bf5eb1f40b26a87aab4dcbfe92a80c2bb
Priya Rajarathinam <cat@jjjjjjj.local>
2019-01-18 21:59:08 -0800

    hi

--- database.js
+++ database.js
@@ -1,11 +1,15 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var databasekey="10SKlZqWe3IkC3ymxUWxzMrjgUfNFuixcuqY10gC";
-var test="https://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=n&max=25&offset=0&api_key=${databasekey}"
+var a = 5;
+var b = 10;
+console.log(`Fifteen is ${a + b}.`);
+var test=`https://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=n&max=25&offset=0&api_key=${databasekey}`;
 var request=new XMLHttpRequest();
 request.open('GET',test);
 request.responseType = 'json';
-console.log("in here 1")
+console.log("in here 1");
 request.onload=function(){
-    var lol= JSON.stringify(request.response);
-    console.log(lol)
+    console.log(" in on load");
+    var lol=request.response;
+    console.log(lol);
 }
