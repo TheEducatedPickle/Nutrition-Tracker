ad3ca1a45ddb7404221a8e6a9d103b2f842d6846
Priya Rajarathinam <cat@jjjjjjj.local>
2019-01-19 00:13:15 -0800

    change

--- fetchData.js
+++ fetchData.js
@@ -1,16 +1,24 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var XMLHttpRequest = require('xhr2');
 var databasekey="10SKlZqWe3IkC3ymxUWxzMrjgUfNFuixcuqY10gC";
 var a = 5;
 var b = 10;
 console.log(`Fifteen is ${a + b}.`);
-var test=`https://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=n&max=25&offset=0&api_key=${databasekey}`;
+var test=` https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=${databasekey}&nutrients=205&nutrients=204&nutrients=208&nutrients=269&ndbno=01009`;
 var request=new XMLHttpRequest();
-request.open('GET',test);
-request.responseType = 'json';
-console.log("in here 1");
-request.onload=getResponse();
 
-function getResponse(){
-    var lol=request.response;
-    console.log(lol);
+request.open('GET',test,true);
+
+request.onload=function(){
+    var response = this.response;
+    var json = JSON.parse(response);
+
+    if (request.status >= 200 && request.status < 400) {
+        console.log(json.report.foods[0].nutrients[0].value)
+      } else {
+        console.log('error');
+      }
+
 }
+
+request.send()
+
