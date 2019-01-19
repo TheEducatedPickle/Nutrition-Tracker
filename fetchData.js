da47db52e1c328b1e577025fed9cf1f7019d2084
Austin Yuan <sax202@gmail.com>
2019-01-19 00:14:15 -0800

    firebase

--- fetchData.js
+++ fetchData.js
@@ -1,16 +1,20 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var databasekey="10SKlZqWe3IkC3ymxUWxzMrjgUfNFuixcuqY10gC";
-var a = 5;
-var b = 10;
-console.log(`Fifteen is ${a + b}.`);
-var test=`https://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=n&max=25&offset=0&api_key=${databasekey}`;
-var request=new XMLHttpRequest();
-request.open('GET',test);
-request.responseType = 'json';
-console.log("in here 1");
-request.onload=getResponse();
+var request = new XMLHttpRequest();
 
-function getResponse(){
-    var lol=request.response;
-    console.log(lol);
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.onload = function () {
+
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      console.log(movie.title);
+    });
+  } else {
+    console.log('error');
+  }
 }
+
+request.send();
