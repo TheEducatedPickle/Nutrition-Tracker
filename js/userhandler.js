b7021d6369a6d6e12e5a8d415eeed63f46f76149
Austin Yuan <sax202@gmail.com>
2019-01-20 06:26:07 -0800

    Added all SQL operations

--- js/userhandler.js
+++ js/userhandler.js
@@ -1,8 +1,9 @@
 var db = openDatabase('mydb', '1.0', 'User database', 2 * 1024 * 1024);
+var admin = 'ajyuan@ucsc.edu'
 
 db.transaction(function (tx) {
     //tx.executeSql('DROP TABLE users');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS users (email unique, password, name)');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS users (email unique, password, name, calories DEFAULT 0, sugar DEFAULT 0, fats DEFAULT 0, carbohydrates DEFAULT 0)');
     //tx.executeSql('CREATE TABLE IF NOT EXISTS nt (email unique, calories, sugar, fats, carbohydrates)')
     tx.executeSql('SELECT * FROM users', [], function (tx, results) {
         var len = results.rows.length, i;
@@ -20,12 +21,12 @@ function addUser(name, email, password) {
         //tx.executeSql('INSERT INTO nt (email, 0, 0, 0, 0)');
         console.log(`Added user ${name}`);
     })
-};
+}
 
 function authUser(email, password) {
     //console.log("email: " + email + "\npassword: " + password);
-    db.transaction(function(tx) {
-        tx.executeSql(`SELECT * FROM users WHERE password =? AND email=?`, [password, email], function(tx, results) {
+    db.transaction(function (tx) {
+        tx.executeSql(`SELECT * FROM users WHERE password =? AND email=?`, [password, email], function (tx, results) {
             var len = results.rows.length, i;
             //console.log(len)
             if (len === 0) {
@@ -36,4 +37,21 @@ function authUser(email, password) {
             }
         })
     })
-};
+}
+
+function updateUser(calories = 0, sugar = 0, fats = 0, carbs = 0, email = admin) {
+    console.log("Values: " + calories + " " + sugar + " " + fats + " " + carbs + " " + email)
+    db.transaction(function (tx) {
+        tx.executeSql('UPDATE users SET calories=calories+?,sugar=sugar+?,fats=fats+?,carbohydrates=carbohydrates+? WHERE email=?', [calories, sugar, fats, carbs, email],
+            function (tx, results) { console.log("Success") }, function (transaction, error) { console.log(error); }
+        );
+    })
+}
+
+function getUserData(email = admin) {
+    db.transaction(function (tx) {
+        tx.executeSql('SELECT calories,sugar,fats,carbohydrates FROM users WHERE email=?', [email], function (tx, results) {
+            console.log(results.rows.item(0));
+        })
+    });
+}
