146dc57b29c77a3b1b755c4b830affae133c0e1e
Austin Yuan <sax202@gmail.com>
2019-01-20 04:25:21 -0800

    Styling

--- js/userhandler.js
+++ js/userhandler.js
@@ -3,7 +3,7 @@ var db = openDatabase('mydb', '1.0', 'User database', 2 * 1024 * 1024);
 db.transaction(function (tx) {
     //tx.executeSql('DROP TABLE users');
     tx.executeSql('CREATE TABLE IF NOT EXISTS users (email unique, password, name)');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS nt (email unique, calories, sugar, fats, carbohydrates)')
+    //tx.executeSql('CREATE TABLE IF NOT EXISTS nt (email unique, calories, sugar, fats, carbohydrates)')
     tx.executeSql('SELECT * FROM users', [], function (tx, results) {
         var len = results.rows.length, i;
         for (i = 0; i < len; i++) {
@@ -17,7 +17,7 @@ function addUser(name, email, password) {
     console.log("name: " + name + "\nemail: " + email + "\npassword: " + password);
     db.transaction(function (tx) {
         tx.executeSql('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, password]);
-        tx.executeSql('INSERT INTO nt (email, 0, 0, 0, 0)');
+        //tx.executeSql('INSERT INTO nt (email, 0, 0, 0, 0)');
         console.log(`Added user ${name}`);
     })
 };
